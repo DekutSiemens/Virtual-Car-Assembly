04501f332e1accb6b79e3b6f552b501f0337071f
Charles Murage <Cmugendi3@gmail.com>
2025-02-26 14:20:54 +0300

    lts
    Welding timeline
    cutting and stamping timeline

--- Assets/Models/ScrollingShader/Scripts/ConveyerBelt.cs
+++ Assets/Models/ScrollingShader/Scripts/ConveyerBelt.cs
@@ -1,126 +1,158 @@
 using UnityEngine;
+using UnityEngine.Splines;
 
 namespace nickmaltbie.ScrollingShader
 {
-    /// <summary>
-    /// Conveyer belt that has a uniform direction and speed and will push objects that collide with it.
-    /// </summary>
     public class ConveyerBelt : MonoBehaviour
     {
-        /// <summary>
-        /// How does this belt move objects, by pushing objects sitting
-        /// on top of the belt or by "moving" the belt forward and pulling
-        /// them forward with the object.
-        /// </summary>
-        public enum BeltForceMode
-        {
-            Push,
-            Pull
-        }
+        public enum BeltForceMode { Push, Pull }
+        public enum RelativeDirection { Up, Down, Left, Right, Forward, Backward, Spline }
 
-        /// <summary>
-        /// Relative direction to face from a local transform.
-        /// </summary>
-        public enum RelativeDirection
-        {
-            Up,
-            Down,
-            Left,
-            Right,
-            Forward,
-            Backward
-        }
+        [SerializeField] private float velocity = 0f;
+        [SerializeField] private RelativeDirection direction = RelativeDirection.Down;
+        [SerializeField] private BeltForceMode beltMode = BeltForceMode.Push;
+        [SerializeField] private Renderer beltRenderer;
+        [SerializeField] private string scrollSpeedProperty = "_ScrollSpeedX";
+        [SerializeField] private AudioSource conveyorAudio;
+        [SerializeField] private SplineContainer conveyorSpline; // Assign a Spline when using Spline mode
 
-        /// <summary>
-        /// Velocity of the conveyer belt in meters per second.
-        /// </summary>
-        [SerializeField]
-        [Tooltip("Velocity of the conveyer belt.")]
-        private float velocity;
-
-        /// <summary>
-        /// Direction that the conveyer belt pushes objects.
-        /// </summary>
-        [SerializeField]
-        [Tooltip("Local direction does this push objects.")]
-        private RelativeDirection direction = RelativeDirection.Down;
-
-        /// <summary>
-        /// Kind of force used to move objects. <see cerf="BeltForceMode" /> for more details on 
-        /// how the belt force modes work.
-        /// </summary>
-        // [SerializeField]
-        // [Tooltip("Belt force mode used to push physics objects in the scene.")]
-        private BeltForceMode beltMode = BeltForceMode.Push;
-
-        /// <summary>
-        /// Rigidbody associated with conveyer belt.
-        /// </summary>
         private Rigidbody body;
-
-        /// <summary>
-        /// Position of the conveyer belt.
-        /// </summary>
         private Vector3 pos;
+        private Material beltMaterial;
 
-        /// <summary>
-        /// Awake event to initialize belt configuration.
-        /// </summary>
         public void Awake()
         {
-            this.body = GetComponent<Rigidbody>();
+            body = GetComponent<Rigidbody>();
             pos = transform.position;
+            if (beltRenderer != null)
+            {
+                beltMaterial = beltRenderer.material;
+            }
+            if (conveyorAudio != null)
+            {
+                conveyorAudio.loop = true;
+                conveyorAudio.Play();
+                conveyorAudio.Pause();
+            }
         }
 
-        /// <summary>
-        /// Fixed update to move the belt and pull objects along with it.
-        /// </summary>
         public void FixedUpdate()
         {
-            if (body != null && beltMode == BeltForceMode.Pull)
+            if (body != null && beltMode == BeltForceMode.Pull && direction != RelativeDirection.Spline)
             {
                 Vector3 movement = velocity * GetDirection() * Time.fixedDeltaTime;
                 transform.position = pos - movement;
                 body.MovePosition(pos);
             }
+            UpdateScrollingShader();
+            UpdateAudio();
         }
 
-        /// <summary>
-        /// When colliding with an object, if it has a rigidbody and is not kinematic, push it forward by the speed of
-        /// the conveyer belt.
-        /// </summary>
-        /// <param name="other">Collision event between the objects.</param>
         public void OnCollisionStay(Collision other)
         {
-            if (other.rigidbody != null && !other.rigidbody.isKinematic && beltMode == BeltForceMode.Push)
+            if (other.rigidbody != null && !other.rigidbody.isKinematic)
             {
-                Vector3 movement = velocity * GetDirection() * Time.deltaTime;
-                other.rigidbody.MovePosition(other.transform.position + movement);
+                if (direction == RelativeDirection.Spline && conveyorSpline != null)
+                {
+                    MoveAlongSpline(other.rigidbody);
+                }
+                else if (beltMode == BeltForceMode.Push)
+                {
+                    Vector3 movement = velocity * GetDirection() * Time.deltaTime;
+                    other.rigidbody.MovePosition(other.transform.position + movement);
+                }
+            }
+        }
+
+        private void MoveAlongSpline(Rigidbody obj)
+        {
+            if (conveyorSpline == null) return;
+
+            float progress = Mathf.Repeat(Time.time * velocity, 1f); // Moves object along the spline
+            Vector3 newPosition = conveyorSpline.EvaluatePosition(progress);
+            obj.MovePosition(newPosition);
+        }
+
+        private void UpdateScrollingShader()
+        {
+            if (beltMaterial != null)
+            {
+                beltMaterial.SetFloat(scrollSpeedProperty, velocity);
+            }
+        }
+
+        private void UpdateAudio()
+        {
+            if (conveyorAudio != null)
+            {
+                if (velocity > 0)
+                {
+                    if (!conveyorAudio.isPlaying)
+                    {
+                        conveyorAudio.UnPause();
+                    }
+                }
+                else
+                {
+                    conveyorAudio.Pause();
+                }
+            }
+        }
+
+        public Vector3 GetDirection()
+        {
+            return direction switch
+            {
+                RelativeDirection.Up => transform.up,
+                RelativeDirection.Down => -transform.up,
+                RelativeDirection.Left => -transform.right,
+                RelativeDirection.Right => transform.right,
+                RelativeDirection.Forward => transform.forward,
+                RelativeDirection.Backward => -transform.forward,
+                _ => transform.forward,
+            };
+        }
+
+        // ------- SIGNAL RECEIVER CONTROL METHODS -------
+
+        /// <summary>
+        /// Set the conveyor belt speed dynamically (for Signal Receiver)
+        /// </summary>
+        /// <param name="speed">New belt speed</param>
+        public void SetBeltSpeed(float speed)
+        {
+            velocity = speed;
+            UpdateScrollingShader();
+            UpdateAudio();
+        }
+
+        /// <summary>
+        /// Stops the conveyor belt (for Signal Receiver)
+        /// </summary>
+        public void StopBelt()
+        {
+            SetBeltSpeed(0);
+        }
+
+        /// <summary>
+        /// Change the conveyor belt direction dynamically (for Signal Receiver)
+        /// </summary>
+        /// <param name="dirIndex">Index of the direction (0=Up, 1=Down, etc.)</param>
+        public void SetDirection(int dirIndex)
+        {
+            if (dirIndex >= 0 && dirIndex < System.Enum.GetValues(typeof(RelativeDirection)).Length)
+            {
+                direction = (RelativeDirection)dirIndex;
             }
         }
 
         /// <summary>
-        /// Get the world space direction that the conveyer belt will push objects.
+        /// Assigns a new spline dynamically (for Signal Receiver)
         /// </summary>
-        /// <returns>The direction in world space of teh conveyer belt, will be 1 unit long.</returns>
-        public Vector3 GetDirection()
+        /// <param name="spline">SplineContainer to assign</param>
+        public void SetSpline(SplineContainer spline)
         {
-            switch (this.direction)
-            {
-                case RelativeDirection.Up:
-                    return transform.up;
-                case RelativeDirection.Down:
-                    return -transform.up;
-                case RelativeDirection.Left:
-                    return -transform.right;
-                case RelativeDirection.Right:
-                    return transform.right;
-                case RelativeDirection.Forward:
-                    return transform.forward;
-                case RelativeDirection.Backward:
-                    return -transform.forward;
-            }
-            return transform.forward;
+            conveyorSpline = spline;
         }
     }
 }
